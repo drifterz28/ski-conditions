c8d4529b71fa0e107de4b4937ff9ea4796acf9f0
Chris Whitney <cwhitney@kollective.com>
2020-01-25 21:25:44 -0800

    Remove console

--- src/bachelor-post.js
+++ src/bachelor-post.js
@@ -55,30 +55,7 @@ module.exports = async function() {
   const resort = await getPageHtml(resortList[3]);
   const nwsReport = await weatherData();
   const data = await template(resort.condition, nwsReport);
-  console.log(JSON.stringify(data))
   request.post(process.env.SLACK_URL, {
     body: JSON.stringify(data)
   });
 };
-
-
-/*
-{
-      "type": "context",
-      "elements": [
-        {
-          "type": "image",
-          "image_url": "http://forecast.weather.gov/newimages/medium/nsn100.png",
-          "alt_text": "notifications warning icon"
-        },
-                {
-          "type": "mrkdwn",
-          "text": "*Today*"
-        },
-        {
-          "type": "mrkdwn",
-          "text": "Snow.  Low around 25. Breezy, with a southwest wind 14 to 23 mph, with gusts as high as 32 mph.  Chance of precipitation is 100%. Total nighttime snow accumulation of 4 to 8 inches possible. "
-        }
-      ]
-    }
-    */
